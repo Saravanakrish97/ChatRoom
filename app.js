c1d0875a244f34427484386cc45109558b9f3835
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2025-04-28 08:02:19 +0530

    Update app.js
    Signed-off-by: Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>

--- app.js
+++ app.js
@@ -22,9 +22,8 @@ var files_array  = [];
 var expiryTime = 8;
 var routineTime = 1;
 
-server.listen(8282);		// server starting on port '8282'
+server.listen(process.env.PORT || 8282);    // server starting on port '8282' or Render provided PORT // cofiguring body-parser
 
-// cofiguring body-parser
 app.use(bodyParser.json({	// setting json limit 	
     limit: 1024 * 10000
 }));
